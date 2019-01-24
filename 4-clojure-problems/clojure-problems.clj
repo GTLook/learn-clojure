b1593bf3f4cd4aa19c9d66512dc35695b19bba7f
Gavin Look <29740344+GTLook@users.noreply.github.com>
2019-01-23 17:06:03 -0800

    more 4-clojure problems

--- 4-clojure-problems/clojure-problems.clj
+++ 4-clojure-problems/clojure-problems.clj
@@ -1,5 +1,24 @@
 
 (fn
-  "expects a list of values and returns the second to last in the list"
+  "Expects a list of values and returns the second to last in the list"
   [[var & input]]
    (if (nil? (second input)) var (recur input)))
+
+(fn
+  "Returns true iff the map contains an entry with that key and its value is nil.
+   Defaults to false if not key not contianed in the map"
+  [given-key map-item]
+  (contains? (nil? (given-key map-item false))))
+
+(fn
+  "Write a function which returns the Nth element from a sequence."
+  [given-list given-integer]
+  (if (= 0 given-integer)
+      (first given-list)
+      (recur (rest given-list) (dec given-integer))))
+
+(fn
+  "write a sequence that returns the number of elelments in the sequence"
+  [given-list]
+  (if (nil? given-list)
+    ))
